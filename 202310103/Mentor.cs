d0fee3eb2315f522e32546ccb375da4c907f2ae2
Petrik Diak <diak@petrik.hu>
2023-10-03 11:01:25 +0200

    Mentor class-ban konstruktorok létrehozása

--- 202310103/Mentor.cs
+++ 202310103/Mentor.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace _202310103
 {
-	internal class Mentor
+	internal class Mentor : Person
 	{
+		private string level;
+
+		public Mentor()
+		{
+			this.level = "intermediate";
+		}
+
+		public Mentor(string name, int age, string gender, string level) : base(name, age, gender)
+		{
+			this.level = level;
+		}
 	}
 }
