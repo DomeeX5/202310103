af3c94624cfcb9540ef90b1f51e41ce491ad31d0
Petrik Diak <diak@petrik.hu>
2023-10-03 10:36:26 +0200

    GetGoal() method, Introduce() method, Person() constructor

--- 202310103/Person.cs
+++ 202310103/Person.cs
@@ -8,5 +8,37 @@ namespace _202310103
 {
 	internal class Person
 	{
+		private string name;
+		private int age;
+		private string gender;
+
+		public Person(string name, int age, string gender)
+		{
+			this.name = name;
+			this.age = age;
+			this.gender = gender;
+		}
+
+		public Person()
+		{
+			this.name = "Jane Doe";
+			this.age = 30;
+			this.gender = "female";
+		}
+
+		public string Name { get => name; set => name = value; }
+		public int Age { get => age; set => age = value; }
+		public string Gender { get => gender; set => gender = value; }
+
+
+		public string GetGoal()
+		{
+			return $"My goal is: Live for the moment!";
+		}
+
+		public string Introduce()
+		{
+			return $"Hi, I'm {name}, a {age} year old {gender}.";
+		}
 	}
 }
