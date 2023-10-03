0f25b23b22f198edd0ce4d8001ce1646870beed4
Petrik Diak <diak@petrik.hu>
2023-10-03 10:55:32 +0200

    Mentor class létrehozása

---
+++ 202310103/Mentor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _202310103
+{
+	internal class Mentor
+	{
+	}
+}
